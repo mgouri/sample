5bdf60ba072ce2379833d98cc513f9008b8a169e
gouri <gouri@mail.com>
2017-10-19 22:16:34 -0700

    modified helloworld file

--- src/main/java/com/int/v4/helloworld.java
+++ src/main/java/com/int/v4/helloworld.java
@@ -5,7 +5,7 @@ public class helloworld {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
         System.out.println("Hai");
-        //System.out.print("Hello");
+        System.out.print("Hello");
     }
 
 }
